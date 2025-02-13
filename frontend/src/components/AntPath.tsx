bca5552bedb147278eafb5d4c06236231c9f0717
André Alves Correia <andre2013aguabranca@gmail.com>
2025-02-13 18:49:20 -0300

    chore: ignore errors

--- frontend/src/components/AntPath.tsx
+++ frontend/src/components/AntPath.tsx
@@ -1,11 +1,14 @@
 import { createElementHook, createPathHook, createContainerComponent } from '@react-leaflet/core'
+// @ts-expect-error a biblioteca não tem typesc
 import { antPath } from 'leaflet-ant-path';
 
+// @ts-expect-error aaa
 function createAntPath(props, context) {
     const instance = antPath(props.positions, props.options)
     return { instance, context: { ...context, overlayContainer: instance } }
 }
 
+// @ts-expect-error aaa
 function updateAntPath(instance, props, prevProps) {
     if (props.positions !== prevProps.positions || props.options !== prevProps.options) {
         instance.setLatLngs(props.positions)
@@ -16,5 +19,5 @@ const useAntPathElement = createElementHook(createAntPath, updateAntPath)
 const useAntPath = createPathHook(useAntPathElement)
 const AntPath = createContainerComponent(useAntPath)
 
-export default AntPath
+export default AntPath;
 // fonte: https://github.com/rubenspgcavalcante/react-leaflet-ant-path/issues/10
