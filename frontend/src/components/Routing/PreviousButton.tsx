c4c8e5622abba39b1b72e54e723614b84c70fce1
André Alves Correia <andre2013aguabranca@gmail.com>
2025-03-21 11:12:56 -0300

    refactor: remove unused components

--- frontend/src/components/Routing/PreviousButton.tsx
+++
@@ -1,9 +0,0 @@
-import type { ButtonHTMLAttributes } from 'react'
-
-const PreviousButton = ({
-  ...rest
-}: ButtonHTMLAttributes<HTMLButtonElement>) => {
-  return <button {...rest}>⬅️</button>
-}
-
-export default PreviousButton
