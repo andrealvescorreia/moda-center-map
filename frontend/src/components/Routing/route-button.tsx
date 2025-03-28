2920bb24ac97fde879ac613262b1a3540ab26855
André Alves Correia <andre2013aguabranca@gmail.com>
2025-03-28 15:30:16 -0300

    fix: add labels to navigation buttons

--- frontend/src/components/Routing/route-button.tsx
+++ frontend/src/components/Routing/route-button.tsx
@@ -8,12 +8,13 @@ const RouteButton = ({ className, ...props }: RouteButtonProps) => {
   return (
     <button
       className={twMerge(
-        'flex items-center justify-center bg-green-primary px-2 h-16 w-16 rounded-md outline-white outline-solid shadow-md hover:bg-green-secondary hover:cursor-pointer transition-colors duration-300',
+        'flex items-center justify-center bg-green-primary px-2 h-16 w-16 rounded-md outline-white outline-solid shadow-md hover:bg-green-secondary hover:cursor-pointer transition-colors duration-300 text-white flex-col gap-1',
         className
       )}
       {...props}
     >
-      <img src={PersonWalking} alt="Iniciar rota" className="w-7" />
+      <img src={PersonWalking} alt="Iniciar rota" className="w-5" />
+      <span className="text-xs">Rota</span>
     </button>
   )
 }
