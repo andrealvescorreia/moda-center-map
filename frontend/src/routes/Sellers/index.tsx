277cea5bcf25df0ccc221a137cb5fdb0abdc428c
André Alves Correia <andre2013aguabranca@gmail.com>
2025-03-28 16:54:57 -0300

    fix: improve weird styles

--- frontend/src/routes/Sellers/index.tsx
+++ frontend/src/routes/Sellers/index.tsx
@@ -29,31 +29,34 @@ export default function Sellers() {
   }, [setLoading])
 
   return (
-    <div className="relative h-full w-full">
+    <div className="h-[100dvh] w-[100dvw]">
       <NavBar />
-      <div className="flex justify-center items-center h-18">
-        <h1 className="text-3xl font-semibold">Vendedores</h1>
-      </div>
-      {sellers.length === 0 && !loading && (
-        <div className="flex justify-center items-center h-full">
-          <p className="text-gray02 text-2xl pt-10">
-            Nenhum vendedor cadastrado
-          </p>
+
+      <div className="w-full">
+        <div className="flex justify-center items-center h-18">
+          <h1 className="text-2xl font-semibold">Vendedores</h1>
         </div>
-      )}
-      <SellerList
-        sellers={sellers}
-        onClick={(id) => navigate(`/sellers/${id}`)}
-      />
-      <NavLink
-        to="/new-seller"
-        className="fixed bottom-24 ml-[50%] transform -translate-x-1/2"
-      >
-        <IconButton className="bg-gray06 h-14 px-8 font-semibold border-none text-green-secondary shadow-lg hover:bg-gray07">
-          <Plus />
-          Adicionar
-        </IconButton>
-      </NavLink>
+        {sellers.length === 0 && !loading && (
+          <div className="flex justify-center items-center h-full">
+            <p className="text-gray02 text-2xl pt-10">
+              Nenhum vendedor cadastrado
+            </p>
+          </div>
+        )}
+        <SellerList
+          sellers={sellers}
+          onClick={(id) => navigate(`/sellers/${id}`)}
+        />
+        <NavLink
+          to="/new-seller"
+          className="fixed bottom-18 ml-[50%] transform -translate-x-1/2"
+        >
+          <IconButton className="bg-gray06 h-14 px-8 font-semibold border-none text-green-secondary shadow-lg hover:bg-gray07">
+            <Plus />
+            Adicionar
+          </IconButton>
+        </NavLink>
+      </div>
     </div>
   )
 }
