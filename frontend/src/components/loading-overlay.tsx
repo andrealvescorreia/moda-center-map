7fa03b7138832904424c37e278e6a108e263ad0b
André Alves Correia <andre2013aguabranca@gmail.com>
2025-08-25 17:59:08 -0300

    Change loading spinner color to green

--- frontend/src/components/loading-overlay.tsx
+++ frontend/src/components/loading-overlay.tsx
@@ -3,7 +3,7 @@ import { CircularProgress } from '@mui/material'
 export default function LoadingOverlay() {
   return (
     <div className="absolute ui z-[99999] top-0 left-0 w-[100dvw] h-[100dvh] flex justify-center items-center bg-[#ffffff7f]">
-      <CircularProgress />
+      <CircularProgress style={{ color: 'green' }} />
     </div>
   )
 }
