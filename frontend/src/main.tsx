e8caf759d780f93dc02a3b0917ae7c57df673ac8
André Alves Correia <andre2013aguabranca@gmail.com>
2025-09-07 12:12:08 -0300

    cleanup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './globals.css'
 import { SnackbarProvider } from 'notistack'
 import App from './App.tsx'
